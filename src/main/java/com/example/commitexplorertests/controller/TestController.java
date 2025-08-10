5c4eb3ad54b151efa6d7842769543620cea9a1e7
Mateus Back <m.back@nextage.com.br>
2025-08-10 12:09:15 -0300

    feat: remove 5 linhas

--- src/main/java/com/example/commitexplorertests/controller/TestController.java
+++ src/main/java/com/example/commitexplorertests/controller/TestController.java
@@ -1,11 +1,6 @@
 ﻿package com.example.commitexplorertests.controller;
 
 public class TestController {
-
-    // This class is a placeholder for test cases related to the controller layer.
-    // It can be used to write unit tests for the controller methods.
-
-    // Example method to demonstrate structure
     public String exampleMethod() {
         return "This is an example method in TestController.";
     }
