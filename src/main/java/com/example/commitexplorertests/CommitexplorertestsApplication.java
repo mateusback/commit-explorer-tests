05ad299e4e0254d866ed6b1c1d1a73ae0692b426
Mateus Back <m.back@nextage.com.br>
2025-08-10 12:03:27 -0300

    first commit

---
+++ src/main/java/com/example/commitexplorertests/CommitexplorertestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.commitexplorertests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommitexplorertestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommitexplorertestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/commitexplorertests/CommitexplorertestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.commitexplorertests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommitexplorertestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
