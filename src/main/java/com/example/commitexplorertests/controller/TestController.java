930994baea0325c717b26458f1b9164c5ac96556
Mateus Back <m.back@nextage.com.br>
2025-08-10 15:36:17 -0300

    feat: remove arquivo de 5 linhas

--- src/main/java/com/example/commitexplorertests/controller/TestController.java
+++
@@ -1,5 +0,0 @@
-﻿package com.example.commitexplorertests.controller;
-
-public class TestController {
-    public String exampleMethod() { return "This is an example method in TestController."; }
-}
